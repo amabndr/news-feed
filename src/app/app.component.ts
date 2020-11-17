3ccbdfda781a309cd559eb487d7e8812218a2d4a
Ranjeeth Kudukolli Rao <ranjeeth.rao@moh.gov.om>
2020-11-17 08:51:15 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feedapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'news-feed';
+}
